ee3c64311b244ae71c76676b515ddbd0d0dd1291
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-05-31 14:06:43 +0200

    feat: initialize Nix flake configuration for the project
    This commit introduces a new flake.nix file that sets up the project dependencies
    and defines the development shell with necessary packages for the Odin environment.

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Odin Devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        name = "ox";
+
+        pkgs = import nixpkgs { inherit system; };
+
+        buildInputs = with pkgs; [
+          odin
+          libGL
+          xorg.libX11
+          openssl
+        ];
+
+        LD_LIBRARY_PATH =
+          with pkgs;
+          "$LD_LIBRARY_PATH:${
+            lib.makeLibraryPath [
+              libGL
+              xorg.libX11
+              openssl
+            ]
+          }";
+      in
+      with pkgs;
+      {
+        packages.default = stdenv.mkDerivation {
+          name = name;
+          src = ./.;
+          buildInputs = buildInputs;
+          LD_LIBRARY_PATH = LD_LIBRARY_PATH;
+          buildPhase = ''
+            odin build . -out:${name}
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${name} $out/bin
+          '';
+        };
+
+        devShells.default = mkShell {
+          buildInputs = buildInputs;
+          LD_LIBRARY_PATH = LD_LIBRARY_PATH;
+        };
+      }
+    );
+}
