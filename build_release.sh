c9f102b983daee16e6d29ac8e46e4e7160a87286
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-06-14 17:49:29 +0200

    chore: bump version

--- build_release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-APP_NAME="ox"
-OUT_DIR="build/release"
-
-mkdir -p "$OUT_DIR"
-odin build . -define="VERSION=$(cat VERSION)" -o:speed -out:$OUT_DIR/$APP_NAME # -strict-style -vet -no-bounds-check
-echo "Release build created in $OUT_DIR"
